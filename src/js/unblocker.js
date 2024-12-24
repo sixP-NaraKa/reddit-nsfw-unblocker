234d56c2803bf63184cac33e355737d50fa29284
sixP-NaraKa <71403526+sixP-NaraKa@users.noreply.github.com>
2024-12-24 02:55:04 +0100

    #4: remove (new) 'untagged blocking' modal and bump to 0.5.1

--- src/js/unblocker.js
+++ src/js/unblocker.js
@@ -34,24 +34,12 @@ function unblock() {
     }
 
     /**
-     * Remove the "untagged blocking" tag, that warns that the content is not reviewed
+     * Remove the "untagged blocking" modal, that warns that the content is not reviewed
      * and it prompts the user to use the Reddit app or go to the home page.
-     * Therefore, this popup seems to only appear on smaller screen sizes, e.g. on mobile.
-     * This can also be emulated by using the browsers devtools and changing the screen size.
-     *
-     * The difference between this popup and the NSFW one, is that this one is always (re)loaded, no matter where the user navigates to:
-     *  - on a /r/subreddit page its a direct child of the main "shreddit-app" element
-     *  - on a post it resides as a child element of "dsa-transparency-modal-provider"
-     *
-     * The main (parent) element we care about is the "shreddit-experience-tree" element.
-     * While this element contains not only the popup in question, we will just delete that whole element anyway,
-     * because the actual content that creates that popup resides a bit deeper in the elements children/shadowRoot.
-     *
-     * A page can contain both the NSFW popup as well as this "shreddit-experience-tree" parent element, but in this case
-     * the "active-experiences" property of the element does not contain "untagged_blocking" and the child element.
-     * Meaning the popup does not appear.
+     * This popup seems to only appear on smaller screen sizes, e.g. on mobile.
+     * This can be replicated by using the browsers devtools and emulating a mobile device.
      */
     const untaggedBlockingElement = document
-        .getElementsByTagName("shreddit-experience-tree")[0];
+        .getElementsByTagName("xpromo-untagged-content-blocking-modal")[0];
     untaggedBlockingElement?.remove();
 }
